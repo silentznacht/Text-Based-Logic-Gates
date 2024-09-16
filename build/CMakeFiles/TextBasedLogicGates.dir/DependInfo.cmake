456ea6fa20bc7d79e6c7c718f62b4d397f132e57
silentznacht <mr.beastlysupreme@gmail.com>
2024-09-16 18:30:42 -0400

    Readme restored

---
+++ build/CMakeFiles/TextBasedLogicGates.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/silentz_nacht/Desktop/ProgrammingProjects/Text-Based-Logic-Gates/libs/Logic.cpp" "CMakeFiles/TextBasedLogicGates.dir/libs/Logic.cpp.o" "gcc" "CMakeFiles/TextBasedLogicGates.dir/libs/Logic.cpp.o.d"
+  "/home/silentz_nacht/Desktop/ProgrammingProjects/Text-Based-Logic-Gates/libs/QuestGen.cpp" "CMakeFiles/TextBasedLogicGates.dir/libs/QuestGen.cpp.o" "gcc" "CMakeFiles/TextBasedLogicGates.dir/libs/QuestGen.cpp.o.d"
+  "/home/silentz_nacht/Desktop/ProgrammingProjects/Text-Based-Logic-Gates/src/main.cpp" "CMakeFiles/TextBasedLogicGates.dir/src/main.cpp.o" "gcc" "CMakeFiles/TextBasedLogicGates.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/TextBasedLogicGates.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TextBasedLogicGates.dir/libs/Logic.cpp.o"
+  "CMakeFiles/TextBasedLogicGates.dir/libs/Logic.cpp.o.d"
+  "CMakeFiles/TextBasedLogicGates.dir/libs/QuestGen.cpp.o"
+  "CMakeFiles/TextBasedLogicGates.dir/libs/QuestGen.cpp.o.d"
+  "CMakeFiles/TextBasedLogicGates.dir/src/main.cpp.o"
+  "CMakeFiles/TextBasedLogicGates.dir/src/main.cpp.o.d"
+  "TextBasedLogicGates"
+  "TextBasedLogicGates.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TextBasedLogicGates.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
